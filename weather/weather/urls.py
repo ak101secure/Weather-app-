e04262d6a394037def5d2bb6835714a1b26de3ff
Afzal khan <afzalkhan01765@gmail.com>
2025-05-31 17:05:58 +0600

    Update urls.py

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('Weather',include('main.urls')),
+    path('weather/',include('main.urls')),
 
 ]
